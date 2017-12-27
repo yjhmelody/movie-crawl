3b245367db9df4476647920edabe2d9ec2048eb0
yjhmelody <465402634@qq.com>
2017-12-28 01:16:07 +0800

    update

--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 # 爬取电影数据
 
+自用程序
+
+已经完成部分，仍然在无脑施工中
+
 获取新电影信息，imdb或者豆瓣 ，格式：
 - movies：movieID movieName genres
 - ratings：UserID movieID Ratings
 - users：UserID sex age occupation
+
--- src/douban-crawl.js
+++ src/douban-crawl.js
@@ -10,6 +10,18 @@ const urls = {
     top250:'http://api.douban.com/v2/movie/top250?count=250&start=100'
 }
 
+const options = {
+    url: 'https://api.github.com/repos/request/request',
+    headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/x',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+    }
+}
+
 // 获取热映中电影
 // movieID | movieName | genres
 function getInThreaters(){
@@ -58,12 +70,9 @@ function getTop250(){
  * @param {String} html
  * @return {Promise} promise with url array
  */
-function getUrl(html, pattern=/https:\/\/movie.douban.com\/subject\/([0-9]{6,9})/g){
+function getUrls(html, pattern=/https:\/\/movie.douban.com\/subject\/([0-9]{6,9})/g){
     // https://movie.douban.com/subject/1291557/?from=subject-page    
-    return Promise.resolve()
-        .then(function(){
-            return html.match(pattern)
-        })
+    return html.match(pattern)
 }
 
 /**
@@ -73,33 +82,42 @@ function getUrl(html, pattern=/https:\/\/movie.douban.com\/subject\/([0-9]{6,9})
  * @param {Number} timeout 
  * @param {Number} depth
  */
-function getMovieUrl(url, arr, timeout=1000, depth=5){ 
-    setTimeout(function(){
-        request.get(url)
+function getMovieInfo(url, infos, timeout=20000, depth=4){ 
+    console.log(arr.length)
+    request.get({
+        url,
+        headers:{}
+    })
         .promise()
         .then(function(html){
-            return getUrl(html)
+            return getUrls(html)
         })
         .then(function(urls){
-            urls = _.uniq(urls)
             // urls = _.uniqBy(urls, 0)
             for(let url of urls){
-                arr.push(url)
-                //递归
+                if(infos[url]){
+                    continue
+                }
+
+                // store the info
+                console.log(url)                        
+                infos[url] = []
                 if (depth > 0){
-                    getMovieInfo(url, arr, timout, depth-1)
+                    setTimeout(function(){
+                        getMovieInfo(url, infos, timeout, depth-1)
+                    }, timeout + timeout * Math.random())
                 }
             }
         })
         .catch(function(err){
             if(err){
-                console.log(err)
+                console.log(err.message)
             }
         })
-    }, timeout)
 }
-
+let infos = []
+getMovieInfo('https://movie.douban.com/subject/1291557/?from=subject-page', infos)
 
 exports.getInThreaters = getInThreaters  
 exports.getTop250 = getTop250
-exports.getMovieUrl = getMovieUrl
+exports.getMovieInfo = getMovieInfo
